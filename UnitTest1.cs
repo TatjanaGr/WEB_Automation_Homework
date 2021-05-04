618c5b4c8666a2f72aeb6582b641bc0c90ba24e2
TatjanaGri <tatjana.grigaitiene@yahoo.com>
2021-05-04 21:12:48 +0300

    Final Code Update to Github

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -17,14 +17,11 @@ namespace WebAutomation_Homework
             driver = new ChromeDriver();
             driver.Url = "http://automationpractice.com/index.php";
             driver.Manage().Window.Maximize();
-
         }
 
         string email = "tatjanasg@123.com";
         string password = "12345";
         string name = "Blouse";
-
-
 
         [Test]
         public void A_SignInTest()
@@ -33,8 +30,8 @@ namespace WebAutomation_Homework
 
             IWebElement accountText = driver.FindElement(By.XPath("//*[@id='center_column']/h1"));
             Assert.AreEqual("MY ACCOUNT", accountText.Text, "Account info is incorrect");
-
         }
+
         [Test]
         public void B_CorrectSignInTest()
         {
@@ -48,105 +45,79 @@ namespace WebAutomation_Homework
         public void C_SearchItemTest()
         {
             Login(email, password);
-
             Search(name);
 
             IWebElement searchItemText = driver.FindElement(By.CssSelector("#center_column > h1 > span.lighter"));
             StringAssert.Contains(name.ToUpper(), searchItemText.Text, "Search item name is incorrect");
         }
+
         [Test]
         public void D_ValidateFoundItemTest()
         {
             Login(email, password);
             Search(name);
+
             IWebElement correctSearchItemName = driver.FindElement(By.XPath("//*[@id='center_column']/ul/li[1]/div/div[2]/h5/a"));
             Assert.AreEqual(name, correctSearchItemName.Text, "Found item name is incorrect");
         }
+
         [Test]
         public void E_BuyingItemTest()
         {
             Login(email, password);
             Search(name);
             FinishBuying();
+
             IWebElement orderConfirmationText = driver.FindElement(By.CssSelector("#center_column > h1"));
             Assert.AreEqual("ORDER CONFIRMATION", orderConfirmationText.Text, "Order confirmation text is incorrect");
         }
+
         [Test]
         public void F_ValidateOrderCompleteTest()
         {
             Login(email, password);
             Search(name);
             FinishBuying();
-            IWebElement succesfulOrderText = driver.FindElement(By.CssSelector("#center_column > p.alert.alert-successn"));
+
+            IWebElement succesfulOrderText = driver.FindElement(By.XPath("//*[@id='center_column']/p[1]"));
             Assert.AreEqual("Your order on My Store is complete.", succesfulOrderText.Text, "Succesful order text is incorrect");
         }
+
         public void Login(string email, string password)
         {
-            System.Threading.Thread.Sleep(6000);
-
             driver.FindElement(By.CssSelector("#header > div.nav > div > div > nav > div.header_user_info > a")).Click();
             driver.FindElement(By.Id("email")).SendKeys(email);
             driver.FindElement(By.Id("passwd")).SendKeys(password);
             driver.FindElement(By.Id("SubmitLogin")).Click();
-
         }
 
         public void Search(string name)
         {
-            System.Threading.Thread.Sleep(6000);
             driver.FindElement(By.Id("search_query_top")).SendKeys(name);
             driver.FindElement(By.Name("submit_search")).Click();
-
         }
 
         public void FinishBuying()
         {
-            //Actions action = new Actions(driver);
-            // IWebElement image = driver.FindElement(By.XPath("//*[@id='center_column']/ul/li[1]/div/div[1]/div/a[1]/img"));
-            // action.MoveToElement(image).Click().Perform();
+            driver.FindElement(By.XPath("//*[@id='list']/a/i")).Click();
+            driver.FindElement(By.XPath("//*[@id='center_column']/ul/li/div/div/div[3]/div/div[2]/a[1]/span")).Click();
+            driver.FindElement(By.XPath("//*[@id='header']/div[3]/div/div/div[3]/div/a")).Click();
 
-            //  Actions builder = new Actions(driver);
-            //  builder.MoveToElement(driver.FindElement(By.XPath("//*[@id='center_column']/ul/li[1]/div/div[2]/div[2]/a[1]"))).Click().Build().Perform();
-            Actions actions = new Actions(driver);
-            actions.SendKeys(Keys.PageDown).Build().Perform();
+           Actions actions = new Actions(driver);
+           actions.SendKeys(Keys.PageDown).Build().Perform();
 
-            System.Threading.Thread.Sleep(5000);
-           
-
-            //IWebElement sizeDropdown = driver.FindElement(By.Xpath("/html/body/div/div[2]/div/div[4]/div/div/div/div[4]/form/div/div[2]/div/fieldset[1]/div/div/select"));
-            //SelectElement selectElement = new SelectElement(sizeDropdown);
-            //selectElement.SelectByText("L");
-            driver.Manage().Timeouts().ImplicitWait = System.TimeSpan.FromSeconds(5);
-            // System.Threading.Thread.Sleep(6000);
-
-           driver.FindElement(By.XPath("//*[@id='center_column']/ul/li/div/div[1]/div/a[1]/img")).Click();
-            System.Threading.Thread.Sleep(5000);
-            driver.SwitchTo().Frame("fancybox-frame1620067149478");
-           
-            // IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
-            // IWebElement element = driver.FindElement(By.Name("Submit"));
-            // js.ExecuteScript("arguments[0].scrollIntoView();", element);
-
-            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-            IWebElement AddToCartButton = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.Name("Submit")));
-
-            driver.FindElement(By.Name("Submit")).Click();
-            driver.FindElement(By.XPath("//*[@id='layer_cart']/div[1]/div[2]/div[4]/a/span")).Click();
-            driver.FindElement(By.XPath("//*[@id='center_column']/p[2]/a[1]/span")).Click();
-            driver.FindElement(By.Name("processAddress")).Click();
-            driver.FindElement(By.Id("cgv")).Click();
-            driver.FindElement(By.Name("processCarrier")).Click();
-            driver.FindElement(By.XPath("//*[@id='HOOK_PAYMENT']/div[2]/div/p/a")).Click();
-            driver.FindElement(By.XPath("//*[@id='cart_navigation']/button/span")).Click();
-
+            driver.FindElement(By.CssSelector("#center_column > p.cart_navigation.clearfix > a.button.btn.btn-default.standard-checkout.button-medium")).Click();
+            driver.FindElement(By.CssSelector("#center_column > form > p > button")).Click();
+            driver.FindElement(By.CssSelector("#uniform-cgv")).Click();
+            driver.FindElement(By.CssSelector("#form > p > button")).Click();
+            driver.FindElement(By.CssSelector("#HOOK_PAYMENT > div:nth-child(2) > div > p > a")).Click();
+            driver.FindElement(By.CssSelector("#cart_navigation > button")).Click();
         }
-
 
         [TearDown]
         public void CloseBrowser()
         {
-
-           // driver.Close();
+            driver.Close();
         }
     }
 }
